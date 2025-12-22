22a0b202b4c8692756a2fc6de1ded3fd13f757ec
mohammad_saquib <saquibkudle08@gmail.com>
2025-12-22 17:19:30 +0530

    Removed the unnecessary code from service

--- src/app/Services/auth.service.ts
+++ src/app/Services/auth.service.ts
@@ -1,56 +1,8 @@
-// import { inject, Injectable } from '@angular/core';
-// import { Router } from '@angular/router';
-// import { Login, LoginResponse } from '../Model/Login';
-// import { BehaviorSubject, Observable, tap } from 'rxjs';
-// import { HttpClient } from '@angular/common/http';
-
-// @Injectable({
-//   providedIn: 'root'
-// })
-// export class AuthService {
-//   private apiUrl: string = "https://localhost:7246/api/Login/login";
-//   private readonly JWT_TOKEN = "JWT_TOKEN";
-//   private isAuthenticated = new BehaviorSubject<boolean>(false);
-//   private http = inject(HttpClient);
-//   private router = inject(Router);
-//   private logedUser?: string;
-
-//   constructor() { }
-
-//   login(userObj: Login): Observable<any> {
-//     return this.http.post<LoginResponse>(this.apiUrl, userObj).pipe(
-//       tap(res => this.doLogin(userObj.username, res.token))
-//     );
-//   }
-
-//   private doLogin(username: string, token: string) {
-//     this.logedUser = username;
-//     this.storeToken(token);
-//     this.isAuthenticated.next(true);
-//   }
-
-//   private storeToken(token: string) {
-//     localStorage.setItem(this.JWT_TOKEN, token);
-//   }
-
-//   logOut() {
-//     localStorage.removeItem(this.JWT_TOKEN);
-//     this.isAuthenticated.next(false);
-//     this.router.navigate(['']);
-//   }
-//   getToken(): string | null {
-//     return localStorage.getItem(this.JWT_TOKEN);
-//   }
-// }
-import { HttpClient } from '@angular/common/http';
 import { Injectable, inject } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 import { Router } from '@angular/router';
 import { BehaviorSubject, Observable, tap } from 'rxjs';
 import { LoginResponse } from '../Model/Login';
-
-// interface LoginResponse {
-//   token: string;
-// }
 @Injectable({
   providedIn: 'root'
 })
